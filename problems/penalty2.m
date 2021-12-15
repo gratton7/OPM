4460c7e72a0a064d1184a7b9cc163ca51dc34db0
Philippe Toint <philippe.toint@unamur.be>
2021-12-15 12:09:31 +0100

    added penalty2 and penalty3

---
+++ problems/penalty2.m
@@ -0,0 +1,147 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function  varargout = penalty2( action, varargin )
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   A penalty function arising from
+%   min{ sum_1^n(x_i-1)^2 subject to sum_1^n x_i^2 = 1/4}.
+%   At the solution, the dense Hessian has n-2 eigenvalues of order a
+%   and two of order 1. The values of a = 1e-5; b = 1 are given by
+%   Moré et al., while Buckley proposed different values.
+%
+%   Source: problem 24 in
+%      J.J. More, B.S. Garbow and K.E. Hillstrom,
+%      "Testing Unconstrained Optimization Software",
+%      ACM Transactions on Mathematical Software, vol. 7(1), pp. 17-41, 1981.
+%   Also problem 110 (p. 80) in
+%      A.R. Buckley,
+%      "Test functions for unconstrained minimization",
+%      TR 1989CS-3, Mathematics, statistics and computing centre,
+%      Dalhousie University, Halifax (CDN), 1989.
+%
+%   If the dimension is unspecified, the default n = 10 is chosen.
+%
+%   Ph. Toint 22 VII 2018.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+st      = dbstack;
+pname   = st.name;
+problem = str2func( pname );
+
+switch ( action )
+
+case 'setup' % varargout = [ x0, fstar, xtype, xlower, xupper, clower, cupper, class, error ]
+
+   if ( length( varargin ) )
+      n = varargin{1};
+      if ( n < 1 )
+         disp( [ ' ERROR in penalty2: n = ', int2str(n), ' must be > 1!' ] )
+      end
+   else
+      n = 10;
+   end
+   varargout{1} = 0.5*ones( n, 1 );             % x0
+   varargout{2} = 'unknown';                    % fstar
+   varargout{3} = '';                           % xtype
+   varargout{4} = [];                           % xlower
+   varargout{5} = [];                           % xupper
+   varargout{6} = [];                           % clower
+   varargout{7} = [];                           % cupper
+   varargout{8} = 'SUR2-AN-V-0';                % class
+
+case 'cpsstr'
+
+   n     = varargin{1};
+   eldom = cell( 2*n, 1 );
+   ie    = 1;
+   for iel = 1:n-1
+      eldom{ ie }   = [ iel iel+1 ];
+      eldom{ ie+1 } = [ iel ];
+      ie = ie + 2;
+   end
+   eldom{ 2*n-1 } = [ 1:n ];
+   eldom{ 2*n   } = [ 1 ];
+   cpsstr.name  = pname;
+   cpsstr.eldom = eldom;
+   cpsstr.param = { n };
+   varargout{1} = cpsstr;
+
+case 'objf'   % varargout = [ f, g, H ]
+
+   x = varargin{1};
+   if ( nargin > 2 && isstruct( varargin{2} ) && isfield( varargin{2}, 'eldom' ) );
+      cpsstr = varargin{2};
+   else
+      cpsstr = problem( 'cpsstr', length( x ) );
+   end
+   varargout = opm_eval_cpsf( pname, 'elobjf', x, cpsstr.eldom, nargout, cpsstr.param{:} );
+
+case 'elobjf' % varargout = [ fiel, giel, Hiel ]
+
+   iel  = varargin{1};
+   x    = varargin{2};
+   n    = varargin{3};
+   a    = 1e-5; b = 1;   % The Moré et al. values
+   if ( iel <= 2*n-2 )
+      switch ( mod( iel, 2 ) )
+      case 1
+         i    = (iel+1)/2;
+	 ci   = exp(i/10) + exp((i-1)/10);
+	 eim1 = exp(x(1)/10);
+	 ei   = exp(x(2)/10);
+	 riel = eim1+ei-ci;
+         varargout{1} = a*riel^2;
+         if ( nargout > 1 )
+            Jiel         = 0.1 * [ eim1; ei ];
+	    varargout{2} = 2*a*Jiel*riel;
+            if ( nargout > 2 )
+	       Hiel = 0.01 * [ eim1, 0;
+	                        0  , ei ];
+	       varargout{3} = 2*a*(Jiel*Jiel.'+riel*Hiel);
+	    end
+	 end
+      case 0
+         i    = iel / 2 + 1;
+	 ci   = exp(-1/10);
+	 ei   = exp(x(1)/10);
+	 riel = ei-ci;
+         varargout{1} = a*riel^2;
+         if ( nargout > 1 )
+	    Jiel = 0.1 * ei;
+	    varargout{2} = 2*a*Jiel*riel;
+            if ( nargout > 2 )
+	       Hiel = 0.01*ei;
+	       varargout{3} = 2*a*(Jiel*Jiel.'+riel*Hiel);
+	    end
+	 end
+      end
+   elseif( iel == 2*n-1 )
+      e1n  = [ 1:n ]';
+      w    = e1n(n:-1:1);
+      riel = b*(w'*(x.^2)-1);
+      varargout{1} = riel^2;
+      if ( nargout > 1 )
+         Jiel = 2*w.*x;
+	 varargout{2} = 2*b*Jiel*riel;
+         if ( nargout > 2 )
+	    Hiel = 2*diag(w);
+	    varargout{3} = 2*b*(Jiel*Jiel.'+riel*Hiel);
+	 end
+      end
+   else
+      riel = x(1) - 1/5;
+      varargout{1} = b*riel^2;
+      if ( nargout > 1 )
+         varargout{2} = 2*b*riel;
+         if ( nargout > 2 )
+	    varargout{3} = 2*b;
+         end
+      end
+   end
+end
+
+return
+
+end
---
+++ problems/penalty3.m
@@ -0,0 +1,159 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function  varargout = penalty3( action, varargin )
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Third penalty problem.
+%
+%   Source: problem 71 (p. 81) in
+%      A.R. Buckley,
+%      "Test functions for unconstrained minimization",
+%      TR 1989CS-3, Mathematics, statistics and computing centre,
+%      Dalhousie University, Halifax (CDN), 1989.
+%
+%   Ph. Toint 26 VII 2018.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+st      = dbstack;
+pname   = st.name;
+problem = str2func( pname );
+
+switch ( action )
+
+case 'setup' % varargout = [ x0, fstar, xtype, xlower, xupper, clower, cupper, class, error ]
+
+   if ( length( varargin ) )
+      n = varargin{1};
+      if ( n < 3 || round( n/2 ) ~= n/2 )
+         disp( [ ' ERROR in penalty3: n = ', int2str(n), ' must be > 2 and even!' ] )
+      end
+   else
+      n = 10;
+   end
+   varargout{1} = zeros( n , 1 );               % x0
+   varargout{2} = 'unknown';                    % fstar
+   varargout{3} = '';                           % xtype
+   varargout{4} = [];                           % xlower
+   varargout{5} = [];                           % xupper
+   varargout{6} = [];                           % clower
+   varargout{7} = [];                           % cupper
+   varargout{8} = 'OUR2-AY-V-0';                % class
+
+case 'cpsstr'
+
+   n            = varargin{1};
+   eldom{ 1 }   = [ 1:n ];
+   eldom{ 2 }   = [ 1:n ];
+   eldom{ 3 }   = [ 1:n ];
+   eldom{ 4 }   = [ 1:n ];
+   eldom{ 5 }   = [ 1:n/2 ];
+   cpsstr.name  = pname;
+   cpsstr.eldom = eldom;
+   cpsstr.param = { n };
+   varargout{1} = cpsstr;
+
+case 'objf'   % varargout = [ f, g, H ]
+
+   x = varargin{1};
+   if ( nargin > 2 && isstruct( varargin{2} ) && isfield( varargin{2}, 'eldom' ) );
+      cpsstr = varargin{2};
+   else
+      cpsstr = problem( 'cpsstr', length( x ) );
+   end
+   varargout = opm_eval_cpsf( pname, 'elobjf', x, cpsstr.eldom, nargout, cpsstr.param{:} );
+
+case 'elobjf' % varargout = [ fiel, giel, Hiel ]
+
+   iel = varargin{1};
+   x   = varargin{2};
+   n   = varargin{3};
+   a   = 0.001;
+   switch ( iel );
+   case 1
+      r  = [ x(1:n-2)+2*x(2:n-1)+10*x(3:n)-ones(n-2,1) ];
+      R  = sum( r.^2 );
+      en = exp(x(n));
+      varargout{1} = a*(1+R*en);
+      if ( nargout > 1 )
+         for i = 1:n-2
+            Jr(1:n,i)= [zeros(i-1,1);1;2;10;zeros(n-i-2,1)];
+	 end
+         gR = 2*Jr*r;
+         varargout{2} = a*(gR*en+R*[zeros(n-1,1);en]);
+         if ( nargout > 2 )
+	    varargout{3} = 2*a*en*Jr*Jr.';
+	    varargout{3}(n,1:n) = varargout{3}(n,1:n)+a*gR.'*en;
+	    varargout{3}(1:n,n) = varargout{3}(1:n,n)+a*gR*en;
+	    varargout{3}(n,n)   = varargout{3}(n,n)  +a*R*en;
+	 end
+      end
+   case 2
+      s    = [ 2*x(1:n-2)+x(2:n-1)-3*ones(n-2,1) ];
+      S    = sum( s.^2 );
+      enm1 = exp(x(n-1));
+      varargout{1} = a*S*enm1;
+      if ( nargout > 1 )
+         for i = 1:n-2
+            Js(1:n,i)= [zeros(i-1,1);2;1;zeros(n-i-1,1)];
+	 end
+         gS = 2*Js*s;
+         varargout{2} = a*(gS*enm1+S*[zeros(n-2,1);enm1;0]);
+         if ( nargout > 2 )
+	    varargout{3} = 2*a*enm1*Js*Js.';
+	    varargout{3}(n-1,1:n) = varargout{3}(n-1,1:n)+a*gS.'*enm1;
+	    varargout{3}(1:n,n-1) = varargout{3}(1:n,n-1)+a*gS*enm1;
+	    varargout{3}(n-1,n-1) = varargout{3}(n-1,n-1)  +a*S*enm1;
+	 end
+      end
+   case 3
+      r  = [ x(1:n-2)+2*x(2:n-1)+10*x(3:n)-ones(n-2,1) ];
+      R  = sum( r.^2 );
+      s    = [ 2*x(1:n-2)+x(2:n-1)-3*ones(n-2,1) ];
+      S    = sum( s.^2 );
+      varargout{1} = a*R*S;
+      if ( nargout > 1 )
+         for i = 1:n-2
+            Jr(1:n,i)= [zeros(i-1,1);1;2;10;zeros(n-i-2,1)];
+	 end
+         gR = 2*Jr*r;
+         for i = 1:n-2
+            Js(1:n,i)= [zeros(i-1,1);2;1;zeros(n-i-1,1)];
+	 end
+         gS = 2*Js*s;
+         varargout{2} = a*(gR*S+R*gS);
+	 if ( nargout > 2 )
+	    HR = 2*Jr*Jr.';
+	    HS = 2*Js*Js.';
+	    varargout{3} = a * ( gR*gS.'+ HR*S + gS*gR.' + HS*R );
+         end
+      end
+   case 4
+      v = x.^2-n*ones(n,1);
+      varargout{1} = sum( v.^2 );
+      if ( nargout > 1 )
+         Jv = 2*diag(x);
+	 varargout{2} = 2*Jv*v;
+         if ( nargout > 2 )
+	    Hv = 2*eye(n);
+	    varargout{3} = 2 * ( Jv*Jv.'+ 2*diag(v) );
+	 end
+      end
+
+   case 5
+      w = x - ones(n/2,1);
+      varargout{1} = sum( w.^2 );
+      if ( nargout > 1 )
+	 varargout{2} = 2 * w;
+         if ( nargout > 2 )
+	    varargout{3} = 2 * eye(n/2);
+	 end
+      end
+   end
+
+end
+
+return
+
+end
